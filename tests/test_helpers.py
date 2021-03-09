89e7ae130063328823ba5643a685816622bdb2a0
Nellrun <daniil.shchesnyak@gmail.com>
2021-03-09 18:06:34 +0300

    test case

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -10,7 +10,8 @@ import pytest
     (60, '1 minutes'),
     (3600, '01:00 hours'),
     (3670, '01:01 hours'),
-    (36000, '10:00 hours')
+    (36000, '10:00 hours'),
+    (36900, '10:15 hours')
 ])
 def test_format_playtime(seconds, expected_string):
     playtime = datetime.timedelta(seconds=seconds)
