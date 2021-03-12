603285d6364ba7ae208363dbbeb3854cae789765
Nellrun <daniil.shchesnyak@gmail.com>
2021-03-12 15:10:48 +0300

    feat helpers: fix playtime format

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -11,7 +11,8 @@ import pytest
     (3600, '01:00 hours'),
     (3670, '01:01 hours'),
     (36000, '10:00 hours'),
-    (36900, '10:15 hours')
+    (36900, '10:15 hours'),
+    (360000, '100:00 hours')
 ])
 def test_format_playtime(seconds, expected_string):
     playtime = datetime.timedelta(seconds=seconds)
