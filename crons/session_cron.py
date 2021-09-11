a4fa9218ce81e76f1a56ad6424c4e289b7aecf5b
Shchesnyak <shchesnyak@yandex-team.ru>
2021-09-11 15:15:18 +0300

    fallback

--- crons/session_cron.py
+++ crons/session_cron.py
@@ -32,35 +32,38 @@ async def main():
     client = xbox_live.client.get_client()
     pg_client = await postgres.client.get_client()
     while True:
-        chats = await pg_client.get_subscribed_chats()
+        try:
+            chats = await pg_client.get_subscribed_chats()
 
-        players = await client.get_players()
-        sessions = await get_active_sessions(pg_client)
+            players = await client.get_players()
+            sessions = await get_active_sessions(pg_client)
 
-        for player in players:
-            if player.online:
-                if player.gamertag not in sessions:
-                    await pg_client.create_new_session(player.gamertag, player.game)
-                    if player.game != 'Home':
-                        for chat in chats:
-                            await bot.send_message(int(chat.chat_id),
-                                                   f'{player.gamertag} is playing {player.game}')
-            if player.gamertag in sessions:
-                session = sessions[player.gamertag]
-                if player.game != session.game:
+            for player in players:
+                if player.online:
+                    if player.gamertag not in sessions:
+                        await pg_client.create_new_session(player.gamertag, player.game)
+                        if player.game != 'Home':
+                            for chat in chats:
+                                await bot.send_message(int(chat.chat_id),
+                                                       f'{player.gamertag} is playing {player.game}')
+                if player.gamertag in sessions:
                     session = sessions[player.gamertag]
-                    await pg_client.end_session(session.id)
+                    if player.game != session.game:
+                        session = sessions[player.gamertag]
+                        await pg_client.end_session(session.id)
 
-                    ended_at = utc.localize(datetime.datetime.utcnow())
-                    session_time = ended_at - session.start_at
+                        ended_at = utc.localize(datetime.datetime.utcnow())
+                        session_time = ended_at - session.start_at
 
-                    formated_time = helpers.format_playtime(session_time)
+                        formated_time = helpers.format_playtime(session_time)
 
-                    for chat in chats:
-                        if session.game != 'Home':
-                            await bot.send_message(int(chat.chat_id),
-                                                   f'{player.gamertag} played {session.game} '
-                                                   f'(session: {formated_time})')
+                        for chat in chats:
+                            if session.game != 'Home':
+                                await bot.send_message(int(chat.chat_id),
+                                                       f'{player.gamertag} played {session.game} '
+                                                       f'(session: {formated_time})')
+        except Exception as exc:
+            print(exc)
         await asyncio.sleep(SLEEP_TIME)
 
 
